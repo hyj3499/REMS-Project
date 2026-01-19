8e29d1525a69db7ec5c8e9a4a5de808b75d9f62b
hyj3499 <qwer97363499@gmail.com>
2026-01-19 20:08:48 +0900

    MainViewModel 클래스 추가 & NuGet 패키지 설치

---
+++ REMS.Client/REMS.Client/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace REMS.Client
+{
+
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
